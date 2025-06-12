678db888eef269a1dc4d12079575b0b653798911
Redfreet <ravigood7@gmail.com>
2025-06-12 17:25:33 +0530

    Weather Api Addition

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,6 +6,34 @@ import { useState } from "react";
 export default function SearchBox() {
   let [city, setCity] = useState("");
 
+  const API_URL = "http://api.openweathermap.org/geo/1.0/direct";
+  const API_KEY = "5cac5b2d1dccb94759714a1475db2654";
+
+  let getWeatherInfo = async () => {
+    let geoResponse = await fetch(
+      `${API_URL}?q=${city}&limit=1&appid=${API_KEY}&units=metric`
+    );
+    let geoData = await geoResponse.json();
+
+    let { lat, lon } = geoData[0];
+
+    let weatherResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    );
+
+    let jsonResponse = await weatherResponse.json();
+
+    let result = {
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelsLike: jsonResponse.main.feels_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
+
   let handleChange = (evt) => {
     setCity(evt.target.value);
   };
@@ -14,6 +42,7 @@ export default function SearchBox() {
     evt.preventDefault();
     console.log(city);
     setCity("");
+    getWeatherInfo();
   };
 
   return (
