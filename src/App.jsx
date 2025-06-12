8688e788a532378bc95c4e19a00ad4122abb1bb7
Redfreet <ravigood7@gmail.com>
2025-06-12 16:04:25 +0530

    Starting files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+// import "./App.css";
+import SearchBox from "./SearchBox";
+
+function App() {
+  return (
+    <>
+      <SearchBox />
+    </>
+  );
+}
+
+export default App;
