41d9c0e0daf2742a2a579aedefd477170b0a1ee4
Redfreet <ravigood7@gmail.com>
2025-06-14 16:07:07 +0530

    Adding card UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 // import "./App.css";
 import SearchBox from "./SearchBox";
+import InfoBox from "./infoBox";
 
 function App() {
   return (
     <>
       <SearchBox />
+      <InfoBox />
     </>
   );
 }
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -24,6 +24,7 @@ export default function SearchBox() {
     let jsonResponse = await weatherResponse.json();
 
     let result = {
+      city: city,
       temp: jsonResponse.main.temp,
       tempMin: jsonResponse.main.temp_min,
       tempMax: jsonResponse.main.temp_max,
